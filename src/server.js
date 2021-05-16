fa5c3b8f041a650dcd79c20bc4c55e6621ee454c
Geunmo Kim <kimgeunmo@gmail.com>
2021-05-16 03:06:14 +0200

    added GET to server.js

--- src/server.js
+++ src/server.js
@@ -4,9 +4,17 @@ const PORT = 5000;
 
 const app = express();
 
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
-  console.log(`Server listening on port ${PORT} 🚀`);
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
 
 // app.listen(port, handler)
+
+// GET is an HTTP method GET me the web page
+
+// HTTP is how servers communicate with each other
