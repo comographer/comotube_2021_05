be6f878185753acd14b054f2aa2f60ca59676473
Geunmo Kim <kimgeunmo@gmail.com>
2021-05-17 22:50:51 +0200

    added middleware

--- src/server.js
+++ src/server.js
@@ -4,15 +4,20 @@ const PORT = 5000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to : ${req.url}`);
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send("I still love you");
+  return res.send("I love middlewares");
 };
 
 const handleLogin = (req, res) => {
   return res.send("Login here:");
 };
 
-app.get("/", handleHome);
+app.get("/", gossipMiddleware, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () =>
@@ -31,3 +36,5 @@ app.listen(PORT, handleListening);
 // res.end() kills the request
 
 // res.send() shows message
+
+// all controllers are middewares and they have three arguments : req, res, next
