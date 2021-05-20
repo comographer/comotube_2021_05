3b34a1922003da2aa53c323c29bae37093d6e235
Geunmo Kim <kimgeunmo@gmail.com>
2021-05-20 20:31:51 +0200

    dirty router code

--- src/server.js
+++ src/server.js
@@ -5,18 +5,29 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
-
-const login = (req, res) => {
-  return res.send("Welcome to the private lounge.");
-};
-
 app.use(logger);
-app.get("/", handleHome);
-app.get("/login", login);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
