fddce75f18034cfc6dd61ad6958fbdacf22cc7df
comographer <kimgeunmo@gmail.com>
2021-05-26 14:38:13 +0200

    Planning Routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
-import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
@@ -11,11 +11,11 @@ const logger = morgan("dev");
 app.use(logger);
 
 app.use("/", globalRouter);
-app.use("/users", userRouter);
 app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
-  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
 
