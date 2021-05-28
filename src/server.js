35c5c344f69a9bbd8dfe18896a12c00913106093
comographer <kimgeunmo@gmail.com>
2021-05-28 16:17:56 +0200

    added pug partials using include

--- src/server.js
+++ src/server.js
@@ -8,9 +8,9 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
-app.set("view engne", "pug");
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
