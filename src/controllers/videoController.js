33e773c08edbf79361be8a825484a828d2d54a00
comographer <kimgeunmo@gmail.com>
2021-05-31 13:16:07 +0200

    addingfake array database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,37 @@
+const videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 123,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "46 minutes ago",
+    views: 5943,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "59 minutes ago",
+    views: 592,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 123,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "46 minutes ago",
-      views: 5943,
-      id: 1,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "59 minutes ago",
-      views: 592,
-      id: 1,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const see = (req, res) => {
-  return res.render("watch", { pageTitle: "Watch" });
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
 };
 export const edit = (req, res) => {
   return res.render("edit", { pageTitle: "Edit" });
