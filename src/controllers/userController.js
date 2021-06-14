13549b11e0b7bd4fdecb8d2f0355d76b7472682d
Geunmo Kim <kimgeunmo@gmail.com>
2021-06-14 20:42:37 +0200

    github oauth config

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -35,6 +35,20 @@ export const postJoin = async (req, res) => {
   }
 };
 
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = (req, res) => {};
+
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
 
