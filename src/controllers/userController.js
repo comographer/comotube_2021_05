9162fc7b537ac9cec8597b525bd26738197d88ab
Geunmo Kim <kimgeunmo@gmail.com>
2021-06-14 21:07:23 +0200

    fetch is not usable in backend

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -47,7 +47,24 @@ export const startGithubLogin = (req, res) => {
   return res.redirect(finalUrl);
 };
 
-export const finishGithubLogin = (req, res) => {};
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "http://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalURL = `${baseUrl}?${params}`;
+  const data = await fetch(finalURL, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const json = await data.json();
+  console.log(json);
+};
 
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
