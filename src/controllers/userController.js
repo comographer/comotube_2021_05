511f0d602d267b62f33b0c5d0d3aa807d63cd7a9
comographer <kimgeunmo@gmail.com>
2021-06-15 13:40:31 +0200

    Login Authentification Done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -88,13 +88,10 @@ export const finishGithubLogin = async (req, res) => {
     if (!emailObj) {
       return res.redirect("/login");
     }
-    const existingUser = await User.findOne({ email: emailObj.email });
-    if (existingUser) {
-      req.session.loggedIn = true;
-      req.session.user = existingUser;
-      return res.redirect("/");
-    } else {
-      const user = await User.create({
+    let user = await User.findOne({ email: emailObj.email });
+    if (!user) {
+      user = await User.create({
+        avatarUrl: userData.avatar_url,
         name: userData.name,
         username: userData.login,
         email: emailObj.email,
@@ -102,10 +99,10 @@ export const finishGithubLogin = async (req, res) => {
         socialOnly: true,
         location: userData.loation,
       });
-      req.session.loggedIn = true;
-      req.session.user = user;
-      return res.redirect("/");
     }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
   } else {
     return res.redirect("/login");
   }
@@ -117,7 +114,7 @@ export const getLogin = (req, res) =>
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
   const pageTitle = "Login";
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ username, socialOnly: false });
   if (!user) {
     return res.status(400).render("login", {
       pageTitle,
@@ -136,10 +133,11 @@ export const postLogin = async (req, res) => {
   return res.redirect("/");
 };
 
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
+
 export const edit = (req, res) => res.send("Edit User");
-
-export const remove = (req, res) => res.send("Remove User");
-
-export const logout = (req, res) => res.send("Log out");
 
 export const see = (req, res) => res.send("See User");
